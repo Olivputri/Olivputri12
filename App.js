ae8c28dfccdcde4232411f083a7b3b3cdcb71e1a
Rival Rahayaan <rahayaanrival403@gmail.com>
2023-11-10 00:19:45 +0700

    mobileproject

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+
 import HomeScreen from "./screens/HomeScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import LoginScreen from "./screens/LoginScreen";
